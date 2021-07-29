f2424e8a8b6163094d0ecc3074f8018237751e03
NARVS1999 <johnpaulnarvasa6@gmail.com>
2021-07-29 10:08:19 +0800

    added html and js button element and function

--- script.js
+++ script.js
@@ -38,3 +38,32 @@ newDiv.appendChild(param);
 
 container.appendChild(newDiv);
 
+function alertFunction () {
+    alert("YAY! YOU DID IT!");
+}
+
+const btn = document.querySelector("#btn");
+// btn.onclick = () => alert("Hello World");
+btn.onclick  = alertFunction;
+
+const btn2 = document.querySelector("#btn2");
+// btn2.addEventListener("click", () => {
+//     alert("Hello World");
+// });
+
+// btn2.addEventListener("click", alertFunction);
+btn2.addEventListener("click", function (e) {
+    console.log(e.target);
+    e.target.style.background = 'pink';
+})
+
+//buttons is a node list. It looks and acts much like a array.
+const buttons = document.querySelectorAll("button");
+//we use the .forEach method to irerate through each button
+buttons.forEach((button) => {
+
+    // and for each one we add a "click" listener.
+    button.addEventListener("click", () => { 
+        alert(button.id);
+    });
+});
